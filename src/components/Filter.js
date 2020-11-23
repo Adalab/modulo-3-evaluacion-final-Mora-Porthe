18d860b9eea0c784739f66944fbfd62182b46d64
Mora Porthe <moraporthe@gmail.com>
2020-11-23 23:54:46 +0100

    add form style

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "../stylesheets/layout/_filter.scss";
 
 const Filter = () => {
   return (
-    <>
+    <form className="form">
       <label className="form__title-name" htmlFor="name">
         ¿Who are you looking for?
       </label>
       <input className="form__input-text" type="text" name="name" id="name" />
-    </>
+    </form>
   );
 };
 
