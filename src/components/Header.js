275c4252de71c01905911a2d4042d88eae3452bf
Mora Porthe <moraporthe@gmail.com>
2020-11-23 19:14:37 +0100

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import logoHeader from "../images/logoRM.png";
+import "../stylesheets/_header.scss";
 
 const Header = () => {
-  return <></>;
+  return (
+    <header className="header" role="banner">
+      <img
+        className="header__logo"
+        src={logoHeader}
+        alt="Rick and Morty Logo"
+        title="Rick and Morty search your character"
+      />
+    </header>
+  );
 };
 
 export default Header;
