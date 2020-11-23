6fe1afddcd1ac165cf17b15fde97e29237005e21
Mora Porthe <moraporthe@gmail.com>
2020-11-23 20:43:08 +0100

    call to api done

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import CharacterList from "./CharacterList";
 import Header from "./Header";
 import Filter from "./Filter";
 import "../stylesheets/App.scss";
+import { useEffect, useState } from "react";
+import getDataFromApi from "../services/getDataFromApi";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    getDataFromApi().then((data) => setCharacters(data));
+  }, []);
+  console.log(characters);
   return (
     <>
       <Header />
       <Filter />
-      <CharacterList />
+      <CharacterList characters={characters} />
     </>
   );
 };
---
+++ src/services/getDataFromApi.js
@@ -0,0 +1,21 @@
+const getDataFromApi = () => {
+  return fetch(
+    "https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      return data.results.map((character) => {
+        return {
+          id: character.id,
+          name: character.name,
+          species: character.species,
+          status: character.status,
+          image: character.image,
+          origin: character.origin.name,
+          episodes: character.episode.length,
+        };
+      });
+    });
+};
+
+export default getDataFromApi;
