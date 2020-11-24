b0d6f09c298655c8c258311d2d9c513c4b3d22fa
Mora Porthe <moraporthe@gmail.com>
2020-11-24 10:02:49 +0100

    events heard

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import getDataFromApi from "../services/getDataFromApi";
 import CharacterList from "./CharacterList";
 import Header from "./Header";
 import Filter from "./Filter";
 import "../stylesheets/App.scss";
-import { useEffect, useState } from "react";
-import getDataFromApi from "../services/getDataFromApi";
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [characterFilter, setCharacterFilter] = useState("");
   useEffect(() => {
     getDataFromApi().then((data) => setCharacters(data));
   }, []);
 
+  // event handlers
+
+  const handleFilter = (data) => {
+    setCharacterFilter(data.value);
+  };
+
+  /* const filteredCharacters; */
+
   return (
     <>
       <Header />
-      <Filter />
+      <Filter handleFilter={handleFilter} />
       <CharacterList characters={characters} />
     </>
   );
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "../stylesheets/layout/_filter.scss";
 
-const Filter = () => {
+const Filter = (props) => {
+  const handleChange = (ev) => {
+    props.handleFilter({
+      value: ev.target.value,
+      key: "character",
+    });
+  };
   return (
     <form className="form">
       <label className="form__title-name" htmlFor="name">
         ¿Who are you looking for?
       </label>
-      <input className="form__input-text" type="text" name="name" id="name" />
+      <input
+        className="form__input-text"
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Search: Beth Smith"
+        onChange={handleChange}
+      />
     </form>
   );
 };
